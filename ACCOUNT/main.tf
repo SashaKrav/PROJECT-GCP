fba5ca1e55acdb432ddd0034d18f9fddef1b9ce8
Alexandr Kravtsov <kravcov4k@gmail.com>
2024-03-11 05:49:07 +0000

    aaaa

---
+++ ACCOUNT/main.tf
@@ -0,0 +1,56 @@
+data "google_billing_account" "acct" {
+  display_name = "My Billing Account"
+  open         = true
+}
+
+resource "random_password" "password" {
+  length  = 16
+  numeric = false
+  special = false
+  lower   = true
+  upper   = false
+}
+
+resource "google_project" "SashaPROJECT" {
+  name            = "SashaPROJECT"
+  project_id      = random_password.password.result
+  billing_account = data.google_billing_account.acct.id
+}
+
+resource "null_resource" "set_project" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "gcloud config set project ${google_project.SashaPROJECT.project_id}"
+  }
+}
+
+resource "null_resource" "unset_project" {
+  provisioner "local-exec" {
+    when    = destroy
+    command = "gcloud config unset project"
+  }
+}
+
+resource "null_resource" "enable_apis" {
+  depends_on = [
+    google_project.SashaPROJECT,
+    null_resource.set_project
+  ]
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      gcloud services enable compute.googleapis.com
+      gcloud services enable dns.googleapis.com
+      gcloud services enable storage-api.googleapis.com
+      gcloud services enable container.googleapis.com
+      gcloud services enable file.googleapis.com
+      gcloud services enable sqladmin.googleapis.com
+    EOT
+  }
+}
